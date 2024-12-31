5158f76291ec26e2f6c3d7e91feddf5cffa2a879
Andy Norwood <2754635+bonzo81@users.noreply.github.com>
2024-12-31 23:48:26 +0000

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,46 @@
+# tests/conftest.py
 import pytest
 from src.api.network_client import NetworkClient
 from src.core.agent import NetworkReActAgent
 
 @pytest.fixture
-def netbox_client():
-    return NetworkClient(
-        base_url='http://netbox.test',
-        api_token='test-token'
-    )
+def mock_api_responses():
+    return {
+        'cable_terminations': {
+            'results': [{
+                'id': 1,
+                'cable': {'id': 1},
+                'device': {'name': 'switch-01'},
+                'interface': {'name': 'eth0'},
+                'site': {'name': 'Site A'}
+            }]
+        },
+        'interfaces': {
+            'results': [{
+                'id': 1,
+                'name': 'eth0',
+                'type': '1000base-t',
+                'enabled': True
+            }]
+        }
+    }
 
 @pytest.fixture
-def librenms_client():
-    return NetworkClient(
-        base_url='http://librenms.test',
-        api_token='test-token'
-    )
+def mock_network_client(mock_api_responses):
+    class MockNetworkClient:
+        def __init__(self, base_url, api_token):
+            self.base_url = base_url
+            self.api_token = api_token
+            self.mock_responses = mock_api_responses
+
+        def get(self, endpoint):
+            if 'cable-terminations' in endpoint:
+                return self.mock_responses['cable_terminations']
+            elif 'interfaces' in endpoint:
+                return self.mock_responses['interfaces']
+            return {}
+
+    return MockNetworkClient
 
 @pytest.fixture
 def semantic_mappings():
@@ -35,5 +61,13 @@ def semantic_mappings():
     }
 
 @pytest.fixture
-def network_agent(netbox_client, librenms_client, semantic_mappings):
-    return NetworkReActAgent(netbox_client, librenms_client, semantic_mappings)
+def network_agent(mock_network_client, semantic_mappings):
+    netbox = mock_network_client(
+        base_url='http://netbox.test',
+        api_token='test-token'
+    )
+    librenms = mock_network_client(
+        base_url='http://librenms.test',
+        api_token='test-token'
+    )
+    return NetworkReActAgent(netbox, librenms, semantic_mappings)
